c2ae96d1c0d8a222f22f73546c0736d2331af190
hsha24008-okamoto <hsha24008@ict.shimanet.ed.jp>
2026-04-15 10:45:10 +0900

    初回コミット

---
+++ script.js
@@ -0,0 +1,13 @@
+    const fortunes =[
+        "大吉💖素敵な出会いがあるかも！",
+        "中吉😃少し有機を出すと良いことが起きる",
+        "小吉　今日はゆっくり過ごそう",
+        "凶　無理せず慎重に行動しよう"
+    ];
+
+    const button =document.getElementById("omikuji-bt")
+    const result = document.getElementById("result")
+    button.addEventListener("click", function () {
+        const randomNumber = Math.floor(Math.random() * fortunes.length)
+   result.textContent= fortunes[(randomNumber)];
+    });
